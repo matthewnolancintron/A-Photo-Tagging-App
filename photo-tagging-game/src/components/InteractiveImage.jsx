3f1b536d93d42afe7258a73004f9deb637c6c4a4
matthew nolan-cintron <matthewnolancintron@gmail.com>
2023-06-29 17:16:00 -0400

    updated component to use canvas element and api instead of image element also solved isses with markers that I was having in the previous version of this component that didn't use the canvas element and api

--- photo-tagging-game/src/components/InteractiveImage.jsx
+++ photo-tagging-game/src/components/InteractiveImage.jsx
@@ -1,94 +1,26 @@
-// The ImageComponent can handle the image rendering,
-// click detection, 
-//and coordinate scaling. 
-//When the user clicks on an item,
-// it can emit an event or pass the relevant
-// data (such as the clicked item's coordinates) to the LevelComponent.  
 import React, { useState, useEffect, useRef } from 'react';
-import '../App.css'
+import '../App.css';
 
-const InteractiveImage = ({ imageUrl, itemData, defaultImageWidth, defaultImageHeight, onItemClick, openMenu, namesOfMarkersToAdd }) => {
+const InteractiveImage = ({
+  imageUrl,
+  itemData,
+  defaultImageWidth,
+  defaultImageHeight,
+  onItemClick,
+  openMenu,
+  namesOfMarkersToAdd
+}) => {
   const [imageSize, setImageSize] = useState({ width: 0, height: 0 });
-  const imageRef = useRef(null);
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const canvasRef = useRef(null);
   const [scaledItemsHitboxes, setScaledItemsHitboxes] = useState([]);
   const [markersData, setMarkersData] = useState([]);
 
-
-
-  useEffect(() => {
-    const filteredMarkers = scaledItemsHitboxes.filter((item) =>
-      namesOfMarkersToAdd.includes(item.name)
-    );
-  
-    const updatedMarkersData = filteredMarkers.map((item) => {
-      const centerX = (item.startX + item.endX) / 2;
-      const centerY = (item.startY + item.endY) / 2;
-  
-      return {
-        name: item.name,
-        styles: {
-          top: centerY,
-          left: centerX,
-        
-        },
-      };
-    });
-  
-    setMarkersData(updatedMarkersData);
-  }, [namesOfMarkersToAdd, scaledItemsHitboxes]);
-  
-
-  /**
-   Get the width and height of the current image dimensions  
-   and use it to set the state variable imageSize 
-   This is needed to calculate the scale factors
-   for calculating the scaled hitboxes for each item
-   */
-  useEffect(() => {
-    const handleImageResize = () => {
-      // Get the width and height of the image
-      const { width, height } = imageRef.current.getBoundingClientRect();
-
-      // Set the image size state
-      setImageSize({ width, height });
-    };
-
-    //  
-    handleImageResize(); // Initialize the size on component mount
-
-    //
-    window.addEventListener('resize', handleImageResize);
-
-    //
-    return () => {
-
-      //
-      window.removeEventListener('resize', handleImageResize);
-    };
-  }, []);
-
-  // Call the calculateScaledHitboxes function
-  // whenever the imageSize or itemData changes
-  useEffect(() => {
-    calculateScaledHitboxes();
-  }, [imageSize, itemData]);
-
-
-  const handleImageResize = () => {
-    // Get the width and height of the image
-    const { width, height } = imageRef.current.getBoundingClientRect();
-
-    // Set the image size state
-    setImageSize({ width, height });
-  };
-
-  // Calculate the scaled hitboxes for each item
   const calculateScaledHitboxes = () => {
-    // Calculate the scale factors for width and height
+    console.log(imageSize,'?')
     const widthScaleFactor = imageSize.width / defaultImageWidth;
     const heightScaleFactor = imageSize.height / defaultImageHeight;
 
-    // Adjust the hitbox coordinates based on the scale factors  
     const updatedScaledCoordinates = itemData.map((item) => {
       const scaledHitbox = {
         name: item.name,
@@ -99,21 +31,99 @@ const InteractiveImage = ({ imageUrl, itemData, defaultImageWidth, defaultImageH
       };
       return scaledHitbox;
     });
+
     setScaledItemsHitboxes(updatedScaledCoordinates);
   };
 
-  const handleImageClick = (event) => {
-    const image = event.target;
-    const imageRect = image.getBoundingClientRect();
-    const x = (event.clientX - imageRect.left)
-    const y = (event.clientY - imageRect.top)
+  useEffect(() => {
+    console.log('new image')
+    setImageLoaded(false);
+    const handleImageLoad = () => {
+      const { width, height } = image;
+      setImageSize({ width, height });
+      setImageLoaded(true);
+    };
 
+    const image = new Image();
+    image.src = imageUrl;
+    image.onload = handleImageLoad;
+
+    return () => {
+      image.onload = null; // Clean up the event listener
+    };
+  }, [imageUrl]);
+
+  useEffect(() => {
+    const handleImageResize = () => {
+      if (canvasRef.current) {
+        const { width, height } = canvasRef.current.getBoundingClientRect();
+        setImageSize({ width, height });
+      }
+    };
+
+    handleImageResize();
+    window.addEventListener('resize', handleImageResize);
+
+    return () => {
+      window.removeEventListener('resize', handleImageResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log('update/set image size after load?')
+    if (imageLoaded) {
+      console.log('hello')
+      if (canvasRef.current) {
+        const { width, height } = canvasRef.current.getBoundingClientRect();
+        setImageSize({ width, height });
+        console.log('updated?',imageSize)
+      }
+    }
+  }, [imageLoaded]);
+
+  useEffect(()=>{
+    calculateScaledHitboxes();
+  },[imageSize]);
+
+  useEffect(() => {
+    updateMarkersData();
+  }, [imageSize, namesOfMarkersToAdd]);
+
+  const updateMarkersData = () => {
+    let dataForMarkers = namesOfMarkersToAdd.map((markerData) => {
+      const scaledHitbox = scaledItemsHitboxes.find((item) => item.name === markerData.name);
+  
+      if (scaledHitbox) {
+        const { startX, startY, endX, endY } = scaledHitbox;
+        const centerX = (startX + endX) / 2;
+        const centerY = (startY + endY) / 2;
+        const leftPercent = `${((centerX / imageSize.width) * 100)}%`;
+        const topPercent = `${((centerY / imageSize.height) * 100)}%`;
+  
+        return {
+          name: markerData.name,
+          styles: {
+            top: topPercent,
+            left: leftPercent,
+          },
+        };
+      }
+  
+      return null; // Handle case where marker name doesn't have a corresponding scaledHitbox
+    });
+  
+    setMarkersData(dataForMarkers.filter(Boolean)); // Remove any null values from the array
+  };
+  
+
+
+  const handleImageClick = (event) => {
+    const canvas = canvasRef.current;
+    const canvasRect = canvas.getBoundingClientRect();
+    const x = event.clientX - canvasRect.left;
+    const y = event.clientY - canvasRect.top;
 
     openMenu(event);
-
-    console.log(x, y, 'coords')
-
-    // Check for matching item coordinates
     checkForMatchingItems({ x, y });
   };
 
@@ -125,52 +135,66 @@ const InteractiveImage = ({ imageUrl, itemData, defaultImageWidth, defaultImageH
         clickedCoords.y >= scaledItemsHitbox.startY &&
         clickedCoords.y <= scaledItemsHitbox.endY
       ) {
-        // Match found, handle the logic for the matched item
-        //call function to send data to level
         handleMatchedItem(scaledItemsHitbox.name);
         break;
       }
     }
   };
 
-  //send the item that was matched to level
   const handleMatchedItem = (itemName) => {
-
-    // Logic for when a matched item is found
-    console.log('Matched item:', itemName);
     onItemClick(itemName);
-
   };
 
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
 
-  //fix marker position it seems to be off by a bit
+    const image = new Image();
+    image.src = imageUrl;
+    image.onload = () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+      drawMarkersOnCanvas(context);
+    };
+
+    return () => {
+      image.onload = null; // Clean up the event listener
+    };
+  }, [markersData, defaultImageWidth, defaultImageHeight]);
+
+  const drawMarkersOnCanvas = (context) => {
+    for (const marker of markersData) {
+      const { left, top } = marker.styles;
+      const canvasWidth = canvasRef.current.width;
+      const canvasHeight = canvasRef.current.height;
+  
+      const markerX = (parseFloat(left) / 100) * canvasWidth;
+      const markerY = (parseFloat(top) / 100) * canvasHeight;
+  
+      context.beginPath();
+      context.arc(markerX, markerY, 20, 0, 2 * Math.PI);
+      context.fillStyle = 'red';
+      context.fill();
+    }
+  };
+  
 
   return (
-    <div id='interactiveImage'>
-      {markersData.map((marker) => (
-        <div
-          key={marker.name}
-          className="marker"
-          style={{position:'absolute', 
-                  left: marker.styles.left,
-                  top: marker.styles.top,
-                  // right:marker.styles.right,
-                  // bottom:marker.styles.bottom,
-                  borderRadius:'50%',
-                  backgroundColor:'red',
-                  zIndex:'1',
-                  width:'10px',
-                  height:'10px',
-                 }}
-        ></div>
-      ))}
-      <img
-        src={imageUrl}
+    <div id="interactiveImage">
+      <canvas
+        ref={canvasRef}
         onClick={handleImageClick}
-        ref={imageRef}
-        style={{ width: '100%', height: 'auto', position: 'relative', objectFit: "contain" }}
-        alt="Game Image"
-        onLoad={handleImageResize}
+        style={{
+          width: '100%',
+          height: 'auto',
+          position: 'relative',
+          objectFit: 'contain',
+          maxWidth: defaultImageWidth,
+          maxHeight: defaultImageHeight
+        }}
+        width={defaultImageWidth}
+        height={defaultImageHeight}
       />
     </div>
   );
