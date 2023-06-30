7059cdef51ad6d512f114076de4651fe87daf66d
matthew nolan-cintron <matthewnolancintron@gmail.com>
2023-06-30 16:15:43 -0400

    added formating for displaying the timer

--- photo-tagging-game/src/components/Timer.jsx
+++ photo-tagging-game/src/components/Timer.jsx
@@ -1,14 +1,24 @@
+import React from 'react';
+import '../App.css';
 
-import React, { useState, useEffect, useRef } from 'react';
-import '../App.css'
+const Timer = ({ timer }) => {
+  const formatTime = (timeInSeconds) => {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
 
+    const formattedHours = String(hours).padStart(2, '0');
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(seconds).padStart(2, '0');
 
+    return `${formattedHours}h:${formattedMinutes}m:${formattedSeconds}s`;
+  };
 
-const Timer = ({timer}) => {
+  const formattedTime = formatTime(timer);
 
   return (
-    <div className='Timer'>
-        {timer}
+    <div className="Timer">
+      {formattedTime}
     </div>
   );
 };
