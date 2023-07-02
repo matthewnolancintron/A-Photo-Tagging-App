2ceffbbcb63b9b48b4e4494371064b76fb6a7cf3
matthew nolan-cintron <matthewnolancintron@gmail.com>
2023-07-02 17:06:54 -0400

    removed duplicate code by placing formatTime into it's own file for better reuseability

---
+++ photo-tagging-game/src/components/formatTime.js
@@ -0,0 +1,11 @@
+export const formatTime = (timeInSeconds) => {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
+
+    const formattedHours = String(hours).padStart(2, '0');
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(seconds).padStart(2, '0');
+
+    return `${formattedHours}h:${formattedMinutes}m:${formattedSeconds}s`;
+  };
